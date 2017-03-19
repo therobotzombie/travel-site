1988aad93f5129510af52d4b275482664212a89e
laura redeker <laura.redeker@campus.lmu.de>
2017-03-19 21:35:23 +0100

    added mobile-menu

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -9,16 +9,26 @@ gulp.task('watch', function() {
             baseDir: "app"
         }
     });
+
     watch('./app/*.html', function() {
         browserSync.reload();
-    })
+    });
+
     watch('./app/assets/styles/**/*.css', function() {
         gulp.start('cssInject');
+    });
+
+    watch('./app/assets/scripts/**/*.js', function() {
+        gulp.start('scriptsRefresh');
     })
 });
 
 // before running the cssInject task, first begin and complete any dependency tasks (styles)
-gulp.task('cssInject', ['styles'], function () {
+gulp.task('cssInject', ['styles'], function() {
     return gulp.src('./app/temp/styles/styles.css')
         .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+    browserSync.reload();
 });
