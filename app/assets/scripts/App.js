49d060c55c636544747cd5d6e1f0b5eb63950380
laura redeker <laura.redeker@campus.lmu.de>
2017-04-02 22:07:16 +0200

    completed sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,13 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 // create objects
 var mobileMenu = new MobileMenu();
+var stickyHeader = new StickyHeader();
 
 // reusing the RevealOnScroll-Class
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+
