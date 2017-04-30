b5b747a2c7680e2bd05e7433de764fffc4f8affc
laura redeker <laura.redeker@campus.lmu.de>
2017-04-30 22:03:11 +0200

    completed the modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,10 +2,12 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 // create objects
 var mobileMenu = new MobileMenu();
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
 
 // reusing the RevealOnScroll-Class
 new RevealOnScroll($(".feature-item"), "85%");
